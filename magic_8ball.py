5cfa637cd9a9404da2c5612541102e4d09bb17fb
Corey <corey.amano@gmail.com>
2022-08-10 07:20:08 -0700

    initial problem description

---
+++ magic_8ball.py
@@ -0,0 +1,13 @@
+# Write a magic8.py Python program that can answer any “Yes” or “No” question with a different fortune each time it executes.
+
+# We’ll be using the following 9 possible answers for our Magic 8-Ball:
+
+# Yes - definitely.
+# It is decidedly so.
+# Without a doubt.
+# Reply hazy, try again.
+# Ask again later.
+# Better not tell you now.
+# My sources say no.
+# Outlook not so good.
+# Very doubtful.
