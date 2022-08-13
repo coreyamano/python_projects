53ad2737440748c047af3853d8958748c85c8b6d
Corey <corey.amano@gmail.com>
2022-08-13 16:19:03 -0700

    add lens slice problem

--- gradebook_project.py
+++ gradebook_project.py
@@ -1,7 +1,8 @@
 # You are a student and you are trying to organize your subjects and grades using Python. Let’s explore what we’ve learned about lists to organize your subjects and scores.
 # Create Some Lists:
 # 1. Create a list called subjects and fill it with the classes you are taking:
-
+last_semester_gradebook = [["politics", 80], [
+    "latin", 96], ["dance", 97], ["architecture", 65]]
 # "physics"
 # "calculus"
 # "poetry"
