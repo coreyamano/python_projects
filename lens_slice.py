c1b652ba5da1b75fc1480b5275ef09d7316997db
Corey <corey.amano@gmail.com>
2022-08-13 16:21:11 -0700

    steps 1-8

--- lens_slice.py
+++ lens_slice.py
@@ -11,6 +11,9 @@
 # "anchovies"
 # "mushrooms"
 
+toppings = ["pepperoni", "pineapple", "cheese", "sausage",
+            "olives", "anchovies", "mushrooms"]
+
 # 2. To keep track of how much each kind of pizza slice costs, create a list called prices that holds the following integer values:
 
 # 2
@@ -20,15 +23,16 @@
 # 2
 # 7
 # 2
-
+prices = [2, 6, 1, 3, 2, 7, 2]
 # 3. Your boss wants you to do some research on $2 slices.
 
 # Count the number of occurrences of 2 in the prices list, and store the result in a variable called num_two_dollar_slices. Print it out.
-
+print(prices.count(2))
 # 4. Find the length of the toppings list and store it in a variable called num_pizzas.
-
+num_pizzas = len(toppings)
+print(num_pizzas)
 # 5. Print the string We sell[num_pizzas] different kinds of pizza!, where[num_pizzas] represents the value of our variable num_pizzas.
-
+print("We sell " + str(num_pizzas) + " different kinds of pizza!")
 # 6. Use the existing data about the pizza toppings and prices to create a new two-dimensional list called pizza_and_prices.
 
 # Each sublist in pizza_and_prices should have one pizza topping and an associated price.
@@ -46,16 +50,18 @@
 
 # [price, topping_name]
 # Note: You don’t need to use your original toppings and prices lists in this exercise. Create a new two-dimensional list from scratch.
-
+pizza_and_prices = [[2, "pepperoni"], [6, "pineapple"], [1, "cheese"], [
+    3, "sausage"], [2, "olives"], [7, "anchovies"], [2, "mushrooms"]]
 # 7.
 # Print pizza_and_prices.
 
 # Does it look the way you expect?
-
+print(pizza_and_prices)
 # Sorting and Slicing Pizzas
 # 8.
 # Sort pizza_and_prices so that the pizzas are in the order of increasing price(ascending).
-
+pizza_and_prices.sort()
+print(pizza_and_prices)
 # 9.
 # Store the first element of pizza_and_prices in a variable called cheapest_pizza.
 
