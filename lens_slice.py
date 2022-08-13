842444a3a5767ba5525cae7b50b195f071ab36b4
Corey <corey.amano@gmail.com>
2022-08-13 16:23:17 -0700

    steps 9-14

--- lens_slice.py
+++ lens_slice.py
@@ -64,16 +64,19 @@ pizza_and_prices.sort()
 print(pizza_and_prices)
 # 9.
 # Store the first element of pizza_and_prices in a variable called cheapest_pizza.
-
+cheapest_pizza = pizza_and_prices[0]
+print(cheapest_pizza)
 # 10.
 # A man walks into the pizza store and shouts “I will have your MOST EXPENSIVE pizza!”
 
 # Get the last item of the pizza_and_prices list and store it in a variable called priciest_pizza.
-
+priciest_pizza = pizza_and_prices[-1]
+print(priciest_pizza)
 
 # 11.
 # It looks like the most expensive pizza from the previous step was our very last "anchovies" slice. Remove it from our pizza_and_prices list since the man bought the last slice.
-
+pizza_and_prices.pop()
+print(pizza_and_prices)
 
 # 12.
 # Since there is no longer an "anchovies" pizza, you want to add a new topping called "peppers" to keep your customers excited about new toppings. Here is what your new topping looks like:
@@ -82,14 +85,17 @@ print(pizza_and_prices)
 # Add the new peppers pizza topping to our list pizza_and_prices.
 
 # Note: Make sure to position it relative to the rest of the sorted data in pizza_and_prices, otherwise our data will not be correctly sorted anymore!
-
+pizza_and_prices.insert(4, [2.5, "peppers"])
+print(pizza_and_prices)
 
 # 13.
 # Three mice walk into the store. They don’t have much money(they’re mice), but they do each want different pizzas.
 
 # Slice the pizza_and_prices list and store the 3 lowest cost pizzas in a list called three_cheapest.
+three_cheapest = pizza_and_prices[0:3]
 
 # 14.
 # Great job! The mice are very pleased and will be leaving you a 5-star review.
 
 # Print the three_cheapest list.
+print(three_cheapest)
