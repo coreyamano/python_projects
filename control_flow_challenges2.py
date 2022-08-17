af3fa6937bbad5d6f2606eab8cf3913ad53119ec
Corey <corey.amano@gmail.com>
2022-08-17 14:40:00 -0700

    set up control flow challenges 2

---
+++ control_flow_challenges2.py
@@ -0,0 +1,40 @@
+# 1. In Range
+# Let’s start the advanced challenge problems by testing if a number falls within a certain range. We will accept three parameters where the first parameter is the number we are testing, the second parameter is the lower bound and the third parameter is the upper bound of our range. These are the steps required:
+
+# Define the function to accept three numbers as parameters
+# Test if the number is greater than or equal to the lower bound and less than or equal to the upper bound
+# If this is true, return True, otherwise, return False
+
+# 2. Same Name
+# We need to write a program that checks different names and determines if they are equal. We need to accept two strings and compare them. Here are the steps:
+
+# Define the function to accept two strings, your_name and my_name
+# Test if the two strings are equal
+# Return True if they are equal, otherwise return False
+
+# 3. Always False
+# There are some situations that you normally want to avoid when programming using conditional statements. One example is a contradiction. This occurs when your condition will always be false no matter what value you pass into it. Let’s create an example of a function that contains a contradiction. It will contain a few steps:
+
+# Define the function to accept a single parameter called num
+# Use a combination of <, > and and to create a contradiction in an if statement.
+# If the condition is true, return True, otherwise return False. The trick here is that because we’ve written a contradiction, the condition should never be true, so we should expect to always return False.
+
+# 4. Movie Review
+# We want to create a function that will help us rate movies. Our function will split the ratings into different ranges and tell the user how the movie was based on the movie’s rating. Here are the steps needed:
+
+# Define our function to accept a single number called rating
+# If the rating is equal to or less than 5, return "Avoid at all costs!"
+# If the rating was less than 9, return "This one was fun."
+# If neither of the if statement conditions were met, return "Outstanding!"
+
+# 5. Max Number
+# For the final challenge, we are going to select which number from three input values is the greatest using conditional statements. To do this, we need to check the different combinations of values to see which number is greater than the other two. Here is what we need to do:
+
+# Define a function that has three input parameters, num1, num2, and num3
+# Test if num1 is greater than the other two numbers
+# If so, return num1
+# Test if num2 is greater than the other two numbers
+# If so, return num2
+# Test if num3 is greater than the other two numbers
+# If so, return num3
+# If there was a tie between the two largest numbers, then return "It's a tie!"
