8efb66f7b647a75a2f7d9dd84349be957c9c7937
Corey <corey.amano@gmail.com>
2022-08-17 08:05:05 -0700

    complete problems 1-5

--- control_flow_challenges1.py
+++ control_flow_challenges1.py
@@ -5,6 +5,11 @@
 # Calculate the result of base to the power of exponent
 # Use an if statement to test if the result is greater than 5000. If it is then return True. Otherwise, return False
 
+def large_power(base, exponent):
+    if base**exponent > 5000:
+        return True
+    else:
+        return False
 
 # 2. Over Budget
 # Let’s say we are trying to save some money and we are watching our budget. We need to make sure that the result of our spending is less than the total amount we have allocated for each of the categories. Our function will accept a parameter called budget which describes our spending limit. The next four parameters describe what we are spending our money on. We need to sum all of our spendings and compare it to the budget. If we have gone over budget, we will return True. Otherwise we return False. Here are the steps we need:
@@ -13,6 +18,13 @@
 # Calculate the sum of the last four parameters
 # Use if and else statements to test if the budget is less than the sum of the calculated sum from the previous step.
 # If the condition is true, return True otherwise return False
+
+
+def over_budget(budget, food_bill, electricity_bill, internet_bill, rent):
+    if budget < food_bill + electricity_bill + internet_bill + rent:
+        return True
+    else:
+        return False
 
 # 3. Twice As Large
 # In this challenge, we will determine if one number is twice as large as another number. To do this, we will compare the first number with two times the second number. Here are the steps:
@@ -23,12 +35,25 @@
 # If num1 is greater then return True otherwise return False
 
 
+def twice_as_large(num1, num2):
+    if num1 > num2 * 2:
+        return True
+    else:
+        return False
+
 # 4. Divisible By Ten
 # To make things a bit more challenging, we are going to create a function that determines whether or not a number is divisible by ten. A number is divisible by ten if the remainder of the number divided by 10 is 0. Using this, we can complete this function in a few steps:
 
 # Define the function header to accept one input num
 # Calculate the remainder of the input divided by 10 (use modulus)
 # Use an if statement to check if the remainder was 0. If the remainder was 0, return True, otherwise, return False
+
+
+def divisible_by_ten(num):
+    if num % 10 == 0:
+        return True
+    else:
+        return False
 
 # 5. Not Sum To Ten
 # Finally, we are going to check if the summation of two inputs does not equal ten. Our function will accept two inputs and add them together. If the two numbers added together are not equal to ten, then we will return True, otherwise, we will return False. Here is what we need to do:
@@ -37,3 +62,10 @@
 # Add the two parameters together
 # Test if the result is not equal to 10
 # If the sum is not equal, return True, otherwise, return False
+
+
+def not_sum_to_ten(num1, num2):
+    if num1 + num2 != 10:
+        return True
+    else:
+        return False
