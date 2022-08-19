3cf4075ac9909aabf6ad562a1424e0236495797a
Corey <corey.amano@gmail.com>
2022-08-19 16:16:37 -0700

    complete challenges 1-5

--- list_challenges.py
+++ list_challenges.py
@@ -6,6 +6,11 @@
 # Append the length of the list to the end of the list
 # Return the modified list
 
+def append_size(lst):
+    the_count = len(lst)
+    lst.append(the_count)
+    return lst
+
 # 2. Append Sum
 # Let’s create a function that calculates the sum of the last two elements of a list and appends it to the end. After doing so, it will repeat this process two more times and return the resulting list. You can choose to use a loop or manually use three lines. Here are the steps we need:
 
@@ -15,12 +20,27 @@
 # Repeat steps 2 and 3 two more times
 # Return the modified list
 
+
+def append_sum(lst):
+    times = 0
+    while times < 3:
+        lst.append(lst[-2]+lst[-1])
+        times += 1
+    return lst
+
 # 3. Larger List
 # Let’s say we are working with two conveyor belts that contain items represented by a numerical ID. If one conveyor belt contains more items than the other, then we need to return the ID of the last item on that belt. In the case where they have the same number of items, return the last item from the first conveyor belt. In our code, we can represent the belts using lists. Here are the steps:
 
 # Define the function to accept two parameters for our two lists of numbers
 # Check if the length of the first list is greater than or equal to the length of the second list
 # If true, then return the last element from the first list. Otherwise, return the last element from the second list
+
+
+def larger_list(lst1, lst2):
+    if len(lst1) >= len(lst2):
+        return lst1[-1]
+    elif len(lst1) < len(lst2):
+        return lst2[-1]
 
 # 4. More Than N
 # Our factory produces a variety of different flavored snacks and we want to check the number of instances of a certain type. We have a conveyor belt full of different types of snacks represented by different numbers. Our function will accept a list of numbers(representing the type of snack), a number for the second parameter(the type of snack we are looking for), and another number as the third parameter(the maximum number of that type of snack on the conveyor belt). The function will return True if the snack we are searching for appears more times than we provided as our third parameter. These are the steps we need:
@@ -29,6 +49,13 @@
 # Count the number of occurrences of item(the second parameter) in lst(the first parameter)
 # If the number of occurrences is greater than n(the third parameter), return True. Otherwise, return False
 
+
+def more_than_n(lst, item, n):
+    if lst.count(item) > n:
+        return True
+    else:
+        return False
+
 # 5. Combine Sort
 # Finally, let’s create a function that combines two different lists together and then sorts them. To do this we can combine the lists with an operation and then sort using a function call. Here are the steps we need to use:
 
@@ -36,3 +63,9 @@
 # Combine the two lists together
 # Sort the result
 # Return the sorted and combined list
+
+
+def combine_sort(lst1, lst2):
+    unsorted_new_list = lst1 + lst2
+    sorted_new_list = sorted(unsorted_new_list)
+    return sorted_new_list
