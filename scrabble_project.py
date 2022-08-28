718a2de911d843397835a684faac15e755f4ecce
Corey <corey.amano@gmail.com>
2022-08-27 17:17:19 -0700

    complete steps 1-10

--- scrabble_project.py
+++ scrabble_project.py
@@ -3,15 +3,21 @@
 
 # There are many ways you can extend this project on your own if you finish and want to get more practice!
 
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+           "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1,
+          3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+
 # Build your Point Dictionary
 # 1.
 # We have provided you with two lists, letters and points. We would like to combine these two into a dictionary that would map a letter to its point value.
 
 # Using a list comprehension and zip, create a dictionary called letter_to_points that has the elements of letters as the keys and the elements of points as the values.
-
+letter_to_points = {key: value for key, value in zip(letters, points)}
 # 2.
 # Our letters list did not take into account blank tiles. Add an element to the letter_to_points dictionary that has a key of " " and a point value of 0.
-
+letter_to_points[" "] = 0
 # 3.
 # We want to create a function that will take in a word and return how many points that word is worth.
 
@@ -29,9 +35,19 @@
 # After the for loop is finished, return point_total.
 
 
+def score_word(word):
+    point_total = 0
+    for letter in word:
+        if letter not in letter_to_points:
+            point_total += 0
+        else:
+            point_total += letter_to_points[letter]
+    return point_total
+
+
 # 7.
 # Let’s test this function! Create a variable called brownie_points and set it equal to the value returned by the score_word() function with an input of "BROWNIE".
-
+brownie_points = (score_word("BROWNIE"))
 # 8.
 # We expect the word BROWNIE to earn 15 points:
 
@@ -40,7 +56,7 @@
 # (3 + 1 + 1 + 4 + 4 + 1 + 1) = 15
 # Let’s print out brownie_points to make sure we got it right.
 
-
+print(brownie_points)
 # 9.
 # Create a dictionary called player_to_words that maps players to a list of the words they have played. This table represents the data to transcribe into your dictionary:
 
@@ -48,11 +64,16 @@
 # BLUE	EARTH	ERASER	ZAP
 # TENNIS	EYES	BELLY	COMA
 # EXIT	MACHINE	HUSKY	PERIOD
-
-
+player_to_words = {
+    'player1': ['BLUE', 'TENNIS', 'EXIT'],
+    'wordNerd': ['EARTH', 'EYES', 'MACHINE'],
+    'LexiCon': ['ERASER', 'BELLY', 'HUSKY'],
+    'ProfReader': ['ZAP', 'COMA', 'PERIOD']
+}
+print(player_to_words)
 # 10.
 # Create an empty dictionary called player_to_points.
-
+player_to_points = {}
 
 # 11.
 # Iterate through the items in player_to_words. Call each player player and each list of words words.
--- strings_challenges_advanced.py
+++ strings_challenges_advanced.py
@@ -1,9 +1,9 @@
-1. Check Name
-You are creating an app that allows users to interact and share their coding project ideas online. The first step is to provide your name in the application and a greeting for other people to see which contains your name. Let’s create a function that is able to check if a user’s name is located within their greeting. We need a function that accepts two parameters, a string for our sentence and a string for a name. The function should return True if the name exists within the string(ignoring any differences in capitalization). Here is what we need to do:
+# 1. Check Name
+# You are creating an app that allows users to interact and share their coding project ideas online. The first step is to provide your name in the application and a greeting for other people to see which contains your name. Let’s create a function that is able to check if a user’s name is located within their greeting. We need a function that accepts two parameters, a string for our sentence and a string for a name. The function should return True if the name exists within the string(ignoring any differences in capitalization). Here is what we need to do:
 
-Define the function to accept two parameters, one string for the sentence and one string for the name
-Convert all of the strings to the same case so we don’t have to worry about differences in capitalization
-Check if the name is within the sentence. If so, then return True. Otherwise, return False
+# Define the function to accept two parameters, one string for the sentence and one string for the name
+# Convert all of the strings to the same case so we don’t have to worry about differences in capitalization
+# Check if the name is within the sentence. If so, then return True. Otherwise, return False
 
 
 def check_for_name(sentence, name):
@@ -13,14 +13,14 @@ def check_for_name(sentence, name):
         return False
 
 
-2. Every Other Letter
-For this next function, we are going to create a function that extract every other letter from a string and returns the resulting string. There are a few different ways you can solve this problem Here are the steps needed for one of the ways:
+# 2. Every Other Letter
+# For this next function, we are going to create a function that extract every other letter from a string and returns the resulting string. There are a few different ways you can solve this problem Here are the steps needed for one of the ways:
 
-Define the function to accept one parameter for the string
-Create a new empty string to hold every other letter from the input string
-Loop through the input string while incrementing by two every time
-Inside the loop, append the character at the current location to the new string we initialized earlier
-Return the new string
+# Define the function to accept one parameter for the string
+# Create a new empty string to hold every other letter from the input string
+# Loop through the input string while incrementing by two every time
+# Inside the loop, append the character at the current location to the new string we initialized earlier
+# Return the new string
 
 
 def every_other_letter(word):
@@ -32,14 +32,14 @@ def every_other_letter(word):
     return every_other
 
 
-3. Reverse
-This one is similar to the last challenge. Instead of selecting every other letter, we want to reverse the entire string. This can be performed in a similar manner, but we will need to modify the range we are using. Here is what we need to do:
+# 3. Reverse
+# This one is similar to the last challenge. Instead of selecting every other letter, we want to reverse the entire string. This can be performed in a similar manner, but we will need to modify the range we are using. Here is what we need to do:
 
-Define the function to accept one parameter for the string
-Create a new empty string to hold the reversed string
-Loop through the input string by starting at the end, and working towards the beginning
-Inside the loop, append the character at the current location to the new string we initialized earlier
-Return the result
+# Define the function to accept one parameter for the string
+# Create a new empty string to hold the reversed string
+# Loop through the input string by starting at the end, and working towards the beginning
+# Inside the loop, append the character at the current location to the new string we initialized earlier
+# Return the result
 
 
 def reverse_string(word):
@@ -51,16 +51,16 @@ def reverse_string(word):
     return reverse_word
 
 
-4. Make Spoonerism
-A Spoonerism is an error in speech when the first syllables of two words are switched. For example, a Spoonerism is made when someone says “Belly Jeans” instead of “Jelly Beans”. We are going to make a function that is similar, but instead of using the first syllable, we are going to switch the first character of two words. Here is what we need to do:
+# 4. Make Spoonerism
+# A Spoonerism is an error in speech when the first syllables of two words are switched. For example, a Spoonerism is made when someone says “Belly Jeans” instead of “Jelly Beans”. We are going to make a function that is similar, but instead of using the first syllable, we are going to switch the first character of two words. Here is what we need to do:
 
-Define the function to accept two parameters for our two words
-Get the first character of the first word and the first character of the second word
-Get the remaining characters of the first word and the remaining characters of the second word.
-Append the first character of the second word to the remaining character of the first word
-Append a space character
-Append the first character of the first word to the remaining characters of the second word.
-Return the result
+# Define the function to accept two parameters for our two words
+# Get the first character of the first word and the first character of the second word
+# Get the remaining characters of the first word and the remaining characters of the second word.
+# Append the first character of the second word to the remaining character of the first word
+# Append a space character
+# Append the first character of the first word to the remaining characters of the second word.
+# Return the result
 
 
 def make_spoonerism(word1, word2):
@@ -70,13 +70,13 @@ def make_spoonerism(word1, word2):
     return spooned
 
 
-5. Add Exclamation
-Let’s say we are writing a program that displays a large message on a blimp and we need to fill in any missing space if a short phrase is provided. Our function will accept a string and if the size is less than 20, it will fill in the remaining space with exclamation marks until the size reaches 20. If the provided string already has a length greater than 20, then we will simply return the original string. Here are the steps:
+# 5. Add Exclamation
+# Let’s say we are writing a program that displays a large message on a blimp and we need to fill in any missing space if a short phrase is provided. Our function will accept a string and if the size is less than 20, it will fill in the remaining space with exclamation marks until the size reaches 20. If the provided string already has a length greater than 20, then we will simply return the original string. Here are the steps:
 
-Define the function to accept one parameter for our string
-Loop while the length of our input string is less than 20
-Inside the loop, append an exclamation mark
-Once done, return the result
+# Define the function to accept one parameter for our string
+# Loop while the length of our input string is less than 20
+# Inside the loop, append an exclamation mark
+# Once done, return the result
 
 
 def add_exclamation(word):
